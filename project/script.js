2ff220fee566064c119a9d37216418e087faa218
Arthur Budkovskiy <artik531@gmail.com>
2022-06-25 21:05:00 +0300

    6th hm is done

--- project/script.js
+++ project/script.js
@@ -6,6 +6,47 @@ function service(url) {
 };
 
 window.onload = () => {
+  Vue.component('search', {
+    template: `
+    <label>
+      <input class="goods-search" type="text" @input="$emit('input', $event.target.value)">
+    </label>
+    `
+  });
+
+  Vue.component('busket', {
+    template: `
+    <div class="busketLayout">
+            <div class="busket">
+               <div class="busket_header">
+                  <h2 class="title">My cart</h2>
+                  <i class="fas fa-thin fa-xmark" @click="$emit('close')"></i>
+               </div>
+               <div class="busket_content"></div>
+            </div>
+         </div>
+    `
+  });
+
+  Vue.component('custom-button', {
+    template: `
+  <button class="cart-button" type="button" @click="$emit('click')">
+  <slot></slot>
+  </button>`
+  });
+
+  Vue.component('good', {
+    props: [
+      "item"
+    ],
+    template: `
+    <div class="goods-item">
+    <img src="images/macbook.png" alt="photo">
+    <h3>{{ item.product_name }}</h3>
+    <p>{{ item.price }}</p>
+    </div>`
+  });
+
   const app = new Vue({
     el: "#root",
     data: {
